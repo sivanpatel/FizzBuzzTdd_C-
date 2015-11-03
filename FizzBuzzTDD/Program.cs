8a6c771524ca5a2246a054632c3f1acdbad8525d
Sivan Patel <sivan.patel@googlemail.com>
2015-11-03 17:49:42 +0000

    Finished implementation off TDD FizzBuzz

--- FizzBuzzTDD/Program.cs
+++ FizzBuzzTDD/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace FizzBuzzTDD
+namespace FizzBuzz
 {
-	class MainClass
+	 class FizzBuzzMain
 	{
-		public static void Main (string[] args)
+		public List<string> GetFizzBuzz(int ceiling)
 		{
-			Console.WriteLine ("Hello World!");
+			var sequence = new List<string>();
+
+			for (var i = 1; i <= ceiling; i++)
+			{
+				sequence.Add(GetNextFizzBuzz(i));
+			}
+			return sequence;
 		}
+
+		public string GetNextFizzBuzz(int position)
+		{
+			var nextFizzBuzz = string.Empty;
+			var isFizzOrBuzz = false;
+
+			if (position % 3 == 0)
+			{
+				nextFizzBuzz = "Fizz";
+				isFizzOrBuzz = true;
+			}
+			else if (position % 5 == 0)
+			{
+				nextFizzBuzz = "Buzz";
+				isFizzOrBuzz = true;
+			}
+			else
+			{
+				nextFizzBuzz = position.ToString();
+			}
+
+			if (isFizzOrBuzz && position % 3 == 0 && position % 5 == 0)
+			{
+				nextFizzBuzz = "FizzBuzz";
+			}
+
+			return nextFizzBuzz;
+		}
+
 	}
 }
--- FizzBuzzTDD/Test.cs
+++ FizzBuzzTDD/Test.cs
@@ -1,11 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 
-namespace FizzBuzzTDD
+namespace FizzBuzz
 {
+	[TestFixture]
 	public class Test
 	{
-		public Test ()
+		[Test]
+	public void TestFizzBuzz_ShouldReturnTwoIntegers()
 		{
+			// Arrange
+			FizzBuzzMain fizbuzz = new FizzBuzzMain ();
+			var expected = new List<string>{ "1", "2" };
+
+			// Act
+			var actual = fizbuzz.GetFizzBuzz (2);
+
+			// Assert
+			CollectionAssert.AreEqual (expected, actual);
+		}
+		[Test]
+		public void TestFizzBuzz_ShouldReturnFirstThreeItems()
+		{
+			FizzBuzzMain fizbuzz = new FizzBuzzMain ();
+			var expected = new List<string>{ "1", "2", "Fizz" };
+			var actual = fizbuzz.GetFizzBuzz (3);
+			CollectionAssert.AreEqual (expected, actual);
+		}
+		[Test]
+		public void TestFizzBuzz_ShouldReturnFirstFiveItems()
+		{
+			FizzBuzzMain fizbuzz = new FizzBuzzMain ();
+			var expected = new List<string>{ "1", "2", "Fizz", "4", "Buzz" };
+			var actual = fizbuzz.GetFizzBuzz (5);
+			CollectionAssert.AreEqual (expected, actual);
+		}
+		[Test]
+		public void TestFizzBuzz_ShouldReturnFizzBuzz()
+		{
+			FizzBuzzMain fizbuzz = new FizzBuzzMain ();
+			var position = 15;
+			var expected = "FizzBuzz";
+
+			var actualFizzBuzzList = fizbuzz.GetFizzBuzz(position);
+			var actual = actualFizzBuzzList.ElementAt(position - 1);
+
+			Assert.AreEqual(expected, actual);
 		}
 	}
 }
